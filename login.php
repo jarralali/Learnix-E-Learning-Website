9869a5c358021e2791cbb75b711cd56fbde3f7be
jarralali <muhammadalijarral31@gmail.com>
2023-11-21 12:37:34 -0800

    login.php v2

--- login.php
+++ login.php
@@ -1,45 +1,62 @@
 <?php
-// Database configuration
-$servername = "localhost";
-$username = "root"; // Change to your MySQL username
-$password = ""; // Change to your MySQL password
-$dbname = "your_database"; // Change to your MySQL database name
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+class DatabaseConnectionFactory {
+    public static function createConnection() {
+        $servername = "localhost";
+        $username = "root"; // Change to your MySQL username
+        $password = ""; // Change to your MySQL password
+        $dbname = "your_database"; // Change to your MySQL database name
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+        return new mysqli($servername, $username, $password, $dbname);
+    }
 }
+
+class Authentication {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function authenticate($email, $password) {
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE email=?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            if (password_verify($password, $user['password']) && $email == $user['email']) {
+                // Valid credentials
+                if ($email == "admin@admin.com") {
+                    return "Admin Logged In!";
+                } else {
+                    return "User Logged In!";
+                }
+            } else {
+                // Invalid password
+                return "Invalid email or password";
+            }
+        } else {
+            // Invalid email
+            return "Invalid email or password";
+        }
+    }
+}
+
+// Usage
+$databaseConnection = DatabaseConnectionFactory::createConnection();
+$authentication = new Authentication($databaseConnection);
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Check against database using prepared statements
-    $stmt = $conn->prepare("SELECT * FROM users WHERE email=?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    $resultMessage = $authentication->authenticate($email, $password);
 
-    if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc();
-        if (password_verify($password, $user['password']) && $email==$user['email']) {
-            // Valid credentials, redirect to welcome.php
-            if($email=="admin@admin.com")
-                echo "Admin Logged In!";
-            else
-                echo "User Logged In!";
-        } else {
-            // Invalid password
-            echo "Invalid email or password";
-        }
-    } else {
-        // Invalid email
-        echo "Invalid email or password";
-    }
+    echo $resultMessage;
 }
 
-$conn->close();
+$databaseConnection->close();
+
 ?>
