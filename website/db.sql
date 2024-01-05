faf5e3e5021cd88ffd1f1455973a251517759a42
Aqdas 115 <aqdassaleem697@gmail.com>
2024-01-06 00:10:17 +0500

    fourth version

---
+++ website/db.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE db.sql;
+
+
+USE db.sql;
+
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
